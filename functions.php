521e5d2c69291221d0c0b3fe72ff7c1cddc3827e
Andrej Hočevar <omeldoid@gmail.com>
2023-03-11 17:55:04 +0100

    theme support woo

--- functions.php
+++ functions.php
@@ -2360,5 +2360,10 @@ function replace_s_by_z( $rules ) {
 } */
 // --------------------------------------- 
 
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
 
 ?>
