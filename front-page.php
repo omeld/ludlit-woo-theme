b590a869dc63b47fb62e117cc5c0a544d485c675
Andrej Hočevar <omeldoid@gmail.com>
2023-03-11 17:47:38 +0100

    prepisano s serverja (pogrešam mailchimp signup)

--- front-page.php
+++ front-page.php
@@ -5,44 +5,17 @@ Template name: kao front page – isto kot index!
 ?>
 <?php get_header('home'); ?>
 <div id="main" class="clearfix">
-<!-- ...................................................... -->
-<?php //get_template_part( 'loop', 'news' ); ?>
-<!-- ...................................................... -->
+  <main id="main" class="site-main" role="main">
 
-<?php //include('mailchimp-subscribe.php');?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
+      <?php the_content(); ?>
 
+    <?php endwhile; // end of the loop. ?>
 
-<!-- mailchimp subscribe -->
-<div class="large-top-margin white-background large-bottom-margin medium-top-padding medium-bottom-padding section-not gray-background stretch-full-width font-size-1" style="">
-	<div class="text-inner">
-		<h5 class="center commentsTitle font-size-2">Bodi udeležen. Sodeluj. Prijavi se na novice.</h5>
-<!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{/*background:#fff*/; clear:left; /*font:14px Helvetica,Arial,sans-serif*/; width:100%;}
-	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-<form action="https://ludliteratura.us18.list-manage.com/subscribe/post?u=372ad152b7eae215b71282dee&amp;id=a7ef4ef08f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="vpiši email" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_372ad152b7eae215b71282dee_a7ef4ef08f" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Prijava" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-<!--End mc_embed_signup-->
-		</div>
-	</div>
-</div>
-<!-- end subscribe -->
+  </main><!-- #main -->
 
 
 
-
-<?php get_template_part( 'loop', 'index' ); ?>
 </div>
 <?php get_footer(); ?>
