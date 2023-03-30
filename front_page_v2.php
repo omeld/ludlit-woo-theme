716289ff8f018c43b09607f6ed927c3c613ef754
regy647 <matic.regally@gmail.com>
2023-03-31 00:14:30 +0200

    manjsi popravki
    izgled glavnega menija, priprave za prenos na block editor

--- front_page_v2.php
+++ front_page_v2.php
@@ -6,16 +6,36 @@ Template name: nov home page visual composer
 <?php get_header('home'); ?>
 
 <div id="main" class="clearfix">
-
-	<?php
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post();
-			the_content();
-		}
-	}
-	?>	
+	<!--BANNER-->
+	
+	<!--PRIKAZ KNJIG-->
+<?php
+	$args = array(
+			'post_type' => 'product',
+			'post_status' => 'publish',
+			'posts_per_page' => 5,
+			'order' => 'DESC',
+			'orderby' => 'date',
+		);
+?>
+			<div class='ludlit_wc_custom_products'>
+				<ul class='products ludlit_wc_custom_products'>
+					<?php
+						$loop = new WP_Query($args);
+						if($loop -> have_posts()) {
+						while ( $loop->have_posts() ) : $loop->the_post();
+							wc_get_template_part( 'content', 'product' );
+						endwhile;
+					}
+					?>
+				</ul>
+			</div>
 </div>
+<!--PRIKAZ POSTOV-->
+
+<!--PRIKAZ REVIJ-->
+
+<!--PRIKAZ INSTAGRAM-->
 
 
 
