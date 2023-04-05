a2bf2709c946ad1f0175bf7ce146f2532316c3a5
Andrej Hočevar <omeldoid@gmail.com>
2023-04-05 21:36:10 +0200

    add stylesheet

--- ludlit_wc_new.php
+++ ludlit_wc_new.php
@@ -9,4 +9,18 @@ add_action('after_setup_theme', 'ludlit_wc_add_block_template_part_support');
 function ludlit_wc_add_block_template_part_support() {
 	add_theme_support('block-template-parts');
 }
+
+// additional style sheet
+add_action('wp_enqueue_scripts', 'ludlit_woo_theme_enqueue_style');
+function ludlit_woo_theme_enqueue_style() {
+	wp_enqueue_style( 
+		'ludlit_woo_theme_style', 
+		get_template_directory_uri() . '/includes/ludlit_woo_theme_style.css', 
+		false, 
+		filemtime(get_template_directory() . '/includes/ludlit_woo_theme_style.css'),
+		'all' 
+	);
+}
+
+
 ?>
