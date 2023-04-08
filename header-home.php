59180e7797a36f654bc0a3e93f263692f00dea54
Andrej Hočevar <omeldoid@gmail.com>
2023-04-08 07:33:35 +0200

    remove bootstrap

--- header-home.php
+++ header-home.php
@@ -2,7 +2,7 @@
 <html lang="sl" xml:lang="sl" xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<!--BOOTSTRAP LINK-->
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 		
 		<script type="text/javascript" src="//use.typekit.net/rha7jnl.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
