3d55b1d5379874692b8af77728e0f7730a467c42
Andrej Hočevar <omeldoid@gmail.com>
2023-04-10 21:39:50 +0200

    try 1 col

--- js/ludlit-wc-gradients.js
+++ js/ludlit-wc-gradients.js
@@ -8,11 +8,20 @@ window.addEventListener("DOMContentLoaded", function () {
     );
     
     function ludlit_wc_set_gradient(element, color_start, color_end = 'var(--ludlit-wc-product-background-color)') {
+        /* was working, now try 1 color
         element.parentNode.style.background = 'radial-gradient('
             + 'circle, '
             + 'rgba(' + color_start.join(',') + ',0.5' + ')' 
             + ' 0%, ' 
             + 'rgba(' + color_end.join(',') + ',0.5' + ')' 
+            + ' 90%'
+            + ')';
+        */
+        element.parentNode.style.background = 'radial-gradient('
+            + 'circle, '
+            + 'rgba(' + color_start.join(',') + ',0.5' + ')' 
+            + ' 0%, ' 
+            + color_end
             + ' 90%'
             + ')';
     }
@@ -38,13 +47,15 @@ window.addEventListener("DOMContentLoaded", function () {
             if (img.complete) {
                 resolve({
                     //color: colorThief.getColor(img, 20),
-                    palette: colorThief.getPalette(img, 1, 20)
+                    //palette: colorThief.getPalette(img, 2, 20)
+                    palette: colorThief.getColor(img, 20)
                 });
             } else {
                 img.addEventListener('load', function() {
                     resolve({
                         //color: colorThief.getColor(img, 20),
-                        palette: colorThief.getPalette(img, 1, 20)
+                        //palette: colorThief.getPalette(img, 2, 20)
+                        palette: colorThief.getColor(img, 20)
                     });
                 });
             }
@@ -58,7 +69,7 @@ window.addEventListener("DOMContentLoaded", function () {
         //const {color, palette} = await ludlit_wc_get_colors(img);
         const { palette } = await ludlit_wc_get_colors(img);
         //ludlit_wc_set_gradient(img, palette[0], palette[1]);
-        ludlit_wc_set_gradient(img, palette[0]);
+        ludlit_wc_set_gradient(img, palette);
     }
 
     //apply to all
