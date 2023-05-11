7a80ac7b3ae7f181ae044dece4859c0412af1181
Andrej Hočevar <omeldoid@gmail.com>
2023-05-11 08:55:20 +0200

    change ga code

--- ludlit_wc_new.php
+++ ludlit_wc_new.php
@@ -51,16 +51,14 @@ function newlitGoogleAnalytics() {
 		}
 	}
 ?>
-<!--<script type="text/plain" class="cc-onconsent-analytics">-->
-<script type="text/javascript" class="not-cc-onconsent-analytics">
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-83E6TL916R"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-	ga('create', 'UA-22395392-2', 'auto');
-	ga('send', 'pageview');
-
+  gtag('config', 'G-83E6TL916R');
 </script>
 <?php
 }
